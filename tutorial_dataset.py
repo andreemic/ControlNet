3ddb5df0cff0cdc5bfaf7ee1805222416fe75313
Your Name <andreemic@gmail.com>
2023-02-24 23:01:41 +0000

    merge

--- tutorial_dataset.py
+++ tutorial_dataset.py
@@ -16,24 +16,29 @@ class MyDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        item = self.data[idx]
+        try:
+            item = self.data[idx]
 
-        source_filename = item['source']
-        target_filename = item['target']
-        prompt = item['prompt']
+            source_filename = item['source']
+            target_filename = item['target']
+            prompt = item['prompt']
 
-        source = cv2.imread('./training/zillow/' + source_filename)
-        target = cv2.imread('./training/zillow/' + target_filename)
+            source = cv2.imread('./training/zillow/' + source_filename)
+            source = cv2.resize(source, (512, 512))
+            target = cv2.imread('./training/zillow/' + target_filename)
+            target = cv2.resize(target, (512, 512))
 
-        # Do not forget that OpenCV read images in BGR order.
-        source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
-        target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
+            # Do not forget that OpenCV read images in BGR order.
+            source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
+            target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
 
-        # Normalize source images to [0, 1].
-        source = source.astype(np.float32) / 255.0
+            # Normalize source images to [0, 1].
+            source = source.astype(np.float32) / 255.0
 
-        # Normalize target images to [-1, 1].
-        target = (target.astype(np.float32) / 127.5) - 1.0
+            # Normalize target images to [-1, 1].
+            target = (target.astype(np.float32) / 127.5) - 1.0
 
-        return dict(jpg=target, txt=prompt, hint=source)
+            return dict(jpg=target, txt=prompt, hint=source)
+        except Exception as e:
+            print(f'failed to load {source_filename} or {target_filename}', e)
 
